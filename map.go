195a6b9e5d0bf8a29a1ba2c3e04f3e5b9cd36475
wenmingxing1 <1793701192@qq.com>
2018-05-14 19:24:04 +0800

    map基本

---
+++ map.go
@@ -0,0 +1,28 @@
+/*map*/
+
+package main 
+import "fmt"
+
+func main() {
+	m := make(map[string]int)	//使用马克创建一个空的map
+
+	m["one"] = 1
+	m["two"] = 2
+	m["three"] = 3
+
+	fmt.Println(m)
+
+	fmt.Println(len(m))
+
+	delete(m, "two")	//删除two
+
+	fmt.Println(m)
+
+	m1 := map[string]int{"one":1, "two":2, "three":3}	//直接创建一个map
+	fmt.Println(m)
+
+	for key,val := range m1 {	//利用for-range结构输出key-value
+		fmt.Printf("%s == > %d", key, val)
+	}
+
+}
