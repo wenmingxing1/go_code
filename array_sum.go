e1af20f23ecd0d89b9505697c6a11560e5eb7dfd
wenmingxing1 <1793701192@qq.com>
2018-05-16 00:15:21 +0800

    输出作为形参，为防止拷贝传入地址

---
+++ array_sum.go
@@ -0,0 +1,15 @@
+package main 
+import "fmt"
+
+func main() {
+	arr := [5]int{1,2,3,4,5}
+	fmt.Println(Sum(&arr)) 
+}
+
+func Sum(a *[5]int) int {
+	var sum int
+	for _,i := range(a) {
+		sum += i
+	}
+	return sum
+}
